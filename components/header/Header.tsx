f81809cd5c880b0ebbdda185012d95e15cdba7bd
ESDLIFE\michaelkcl <michaelkclee@esdlife.com>
2025-12-06 13:57:39 +0800

    update

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,22 +1,55 @@
 "use client";
 
-import { Icon } from "@/shared/UI/icon/Icon";
 import { SearchInput } from "../searchInput/SearchInput";
 import styles from "./Header.module.scss";
+import { useEffect, useRef } from "react";
+import { Image } from "@/shared/UI/image/Image";
 
 export const Header = () => {
+  const headerRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const header = headerRef.current;
+
+    if (!header) return;
+
+    const sticky = header.offsetTop;
+
+    const handler = () => {
+      if (window.pageYOffset > sticky) {
+        header.classList.add(styles["sticky"]);
+      } else {
+        header.classList.remove(styles["sticky"]);
+      }
+    };
+
+    handler();
+
+    window.addEventListener("scroll", handler);
+
+    return () => {
+      window.removeEventListener("scroll", handler);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
-      <Icon icon={"logo"} />
-      <SearchInput />
-      <div>
+    <header className={styles.header} ref={headerRef}>
+      <div className={styles.container}>
+        <Image
+          src={`${process.env.IMAGE_PATH}/logo.png`}
+          alt=""
+          className={styles.logo}
+        />
+        <SearchInput />
         <div>
-          <button></button>
-          <button></button>
-          <button></button>
+          <div>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+          </div>
           <button></button>
         </div>
-        <button></button>
       </div>
     </header>
   );
