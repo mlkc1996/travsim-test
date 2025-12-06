253b7b4be6b1f4878ef147b06149e7d2401c8bf6
ESDLIFE\michaelkcl <michaelkclee@esdlife.com>
2025-12-06 11:57:12 +0800

    update

--- components/heroSection/HeroSection.tsx
+++ components/heroSection/HeroSection.tsx
@@ -90,52 +90,42 @@ const HeroHeader = () => {
 
 const Packages = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-[1.6rem]">
       <h4 className="text-level-normal-200 text-font-x-bold">
         Choose your USA eSIM package
       </h4>
-      <div>
-        {packages.map(({ volumn, days, price }, index) => {
-          return (
-            <div
-              key={index}
-              className={`${index === 1 ? styles.best : ""} ${styles.package}`}
-            >
-              <div>
-                <div>
-                  <Icon icon={""} />
-                  <p className="text-level-normal-100 text-font-x-bold">
-                    {volumn}
-                  </p>
-                </div>
-                <div>
-                  <Icon icon={""} />
-                  <p className="text-level-normal-100 text-font-x-bold">
-                    {days} Days
-                  </p>
-                </div>
-              </div>
-            </div>
-          );
+      <div className={styles.packages}>
+        {packages.map((item, index) => {
+          return <Package key={index} {...item} isBest={index === 1} />;
         })}
       </div>
     </div>
   );
 };
 
-const Package = ({ isBest, volume, days }: any) => {
+const Package = ({ isBest, volumn, days, price }: any) => {
   return (
     <div className={`${isBest ? styles.best : ""} ${styles.package}`}>
-      <div>
-        <div>
-          <Icon icon={""} />
-          <p className="text-level-normal-100 text-font-x-bold">{volume}</p>
+      {isBest && (
+        <>
+          <div className={`${styles["best-value-tag"]} text-font-x-bold`}>
+            Best value
+          </div>
+          <div className={`${styles["best-value-bg"]}`}></div>
+          <div className={`${styles["best-value-fore-bg"]}`}></div>
+        </>
+      )}
+      <div className={styles["package-stat"]}>
+        <div className="flex gap-[8px] items-center">
+          <Icon icon={"switch-vertical-01"} />
+          <p className="text-level-normal-100 text-font-x-bold">{volumn}</p>
         </div>
-        <div>
-          <Icon icon={""} />
-          <p className="text-level-normal-100 text-font-x-bold">{days} Days</p>
+        <div className="flex gap-[8px] items-center">
+          <Icon icon={"calendar"} />
+          <p className="text-level-normal-100 text-font-normal">{days} Days</p>
         </div>
       </div>
+      <span className="text-font-x-bold text-level-normal">{price}</span>
     </div>
   );
 };
